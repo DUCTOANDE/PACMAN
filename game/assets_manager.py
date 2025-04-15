638857dd33e47e7d9265ee5e95b4384d9d888bb1
DUCTOANDE <ndtoande@gmail.com>
2025-04-15 21:00:09 +0700

    Update Boards

--- game/assets_manager.py
+++ game/assets_manager.py
@@ -8,18 +8,20 @@ class AssetManager:
         # Load map tiles
         self.image_maps = {}
         for i in range(0, 3):
-            img = pygame.image.load(rf"C:\PACMAN\assets\Image\{i}.png")
+            img = pygame.image.load(rf"E:\School\AI\PACMAN\assets\Image\{i}.png")
             self.image_maps[i] = pygame.transform.scale(img, (CELL_SIZE, CELL_SIZE))
-
+        for i in range(5, 7):
+            img = pygame.image.load(rf"assets/other_images/{i}.png")
+            self.image_maps[i] = pygame.transform.scale(img, (CELL_SIZE, CELL_SIZE))
         # Load screens
-        self.loading_img = pygame.image.load(r"C:\PACMAN\assets\Image\LoadingGame.png")
+        self.loading_img = pygame.image.load(r"E:\School\AI\PACMAN\assets\Image\LoadingGame.png")
         self.loading_img = pygame.transform.scale(self.loading_img, (WIDTH, HEIGHT))
 
-        self.menu_img = pygame.image.load(r"C:\PACMAN\assets\Image\BackgroundPlay.png")
+        self.menu_img = pygame.image.load(r"E:\School\AI\PACMAN\assets\Image\BackgroundPlay.png")
         self.menu_img = pygame.transform.scale(self.menu_img, (WIDTH, HEIGHT))
 
         # Load play button
-        self.play_button_img = pygame.image.load(r'C:\PACMAN\assets\Image\button_play.png')
+        self.play_button_img = pygame.image.load(r'E:\School\AI\PACMAN\assets\Image\button_play.png')
         self.play_button_normal = pygame.transform.scale(self.play_button_img, (100, 60))
         self.play_button_hover = pygame.transform.scale(self.play_button_img, (int(100 * 1.1), int(60 * 1.1)))
         
@@ -37,3 +39,9 @@ class AssetManager:
             img = pygame.image.load(rf"E:\School\AI\PACMAN\assets\player_images\{i}.png")
             scaled_img = pygame.transform.scale(img, (47, 47))
             self.player_images.append(scaled_img)
+        
+        # Load other images
+        self.other_images1 = pygame.image.load(r"E:\School\AI\PACMAN\assets\other_images\5.png")
+        self.other_images1 = pygame.transform.scale(self.other_images1, (CELL_SIZE, CELL_SIZE))
+        self.other_images2 = pygame.image.load(r"E:\School\AI\PACMAN\assets\other_images\6.png")
+        self.other_images2 = pygame.transform.scale(self.other_images2, (CELL_SIZE, CELL_SIZE))
